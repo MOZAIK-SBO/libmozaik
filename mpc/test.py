45480bd730768f0386cc6ed57dbe7b2062663dca
Erik Pohle <erik.pohle@esat.kuleuven.be>
2024-02-08 16:52:27 +0100

    Added python functions dist_enc and dist_dec; Added tests

--- mpc/test.py
+++ mpc/test.py
@@ -1,7 +1,13 @@
 import unittest
 from Crypto.Cipher import AES
+from pathlib import Path
 
 from key_share import MpcPartyKeys, decrypt_key_share, prepare_params_for_dist_enc
+from rep3aes import Rep3AesConfig, dist_enc, dist_dec
+
+import secrets
+import subprocess
+from threading import Thread
 
 class TestDecryptKeyShare(unittest.TestCase):
     def get_config(party_index):
@@ -61,6 +67,135 @@ class TestDecryptKeyShare(unittest.TestCase):
         print(f'M: {result_bytes.hex()}')
         print(f'CT: {ct.hex()}')
         print(f'Tag: {tag.hex()}')
+        
+    def test_aes_gcm_testvectors(self):
+        key = bytes.fromhex("8ca86d5cf34aaf3f271f758df5a29298")
+        nonce = bytes.fromhex("18a04c8f66bdec6a74513af6")
+        instance = AES.new(key=key, mode=AES.MODE_GCM, nonce=nonce)
+        message = bytes.fromhex("00112233445566778899aabbccddeeff")
+        ct, tag = instance.encrypt_and_digest(message)
+        print(f'ct: {ct.hex()}')
+        print(f'tag: {tag.hex()}')
+        
+class TestRep3Aes(unittest.TestCase):
+    def setUp(self):
+        bin_path = Path('rep3aes/target/release/rep3-aes')
+        # run cargo to compile Rep3Aes
+        subprocess.run(['cargo', 'build', '--release', '--bin', 'rep3-aes'], cwd='./rep3aes/', check=True)
+        self.rep3aes_bin = str(bin_path)
+    
+    def run_dist_enc(return_val, party, path_to_bin, key_share, message_share, user_id, analysis_type, computation_id):
+        if party in [0,1,2]:
+            config = f'rep3aes/p{party+1}.toml'
+        else:
+            assert False
+        rep3aes_config = Rep3AesConfig(config, path_to_bin)
+        keys = MpcPartyKeys(TestDecryptKeyShare.get_config(party))
+        
+        ct = dist_enc(rep3aes_config, keys, user_id, computation_id, analysis_type, key_share, message_share)
+        return_val[party] =  ct
+
+    def secret_share(data):
+        r1 = secrets.token_bytes(len(data))
+        r2 = secrets.token_bytes(len(data))
+        r3 = bytearray(len(data))
+        for i in range(len(data)):
+            r3[i] = data[i] ^ r1[i] ^ r2[i]
+        return r1, r2, r3
+
+    def encode_ring_elements(elements):
+        result_bytes = bytearray(len(elements) * 8)
+        for (i, res) in enumerate(elements):
+            for j in range(8):
+                result_bytes[8*i+j] = (res >> 8*j) & 0xff
+        return result_bytes
+
+    def test_dist_enc(self):
+        # the (plaintext) prediction is a vector of 5 64-bit values in little endian
+        result = [6149648890722733960, 3187258121416518661, 3371553381890320898, 1292927509834657361, 1216049165532225112]
+        # as bytes
+        result_bytes = TestRep3Aes.encode_ring_elements(result)
+
+        user_id = "4d14750e-2353-4d30-ac2b-e893818076d2"
+        analysis_type = "Heartbeat-Demo-1"
+        computation_id = "28341f07-286a-4761-8fde-220b7be3d4cc"
+
+        # create key and message shares
+        k1, k2, k3 = TestRep3Aes.secret_share(TestDecryptKeyShare.expected_key)
+        m1, m2, m3 = TestRep3Aes.secret_share(result_bytes)
+
+        return_dict = dict()
+
+        t1 = Thread(target=TestRep3Aes.run_dist_enc, args=[return_dict, 0, self.rep3aes_bin, k1, m1, user_id, analysis_type, computation_id])
+        t2 = Thread(target=TestRep3Aes.run_dist_enc, args=[return_dict, 1, self.rep3aes_bin, k2, m2, user_id, analysis_type, computation_id])
+        t3 = Thread(target=TestRep3Aes.run_dist_enc, args=[return_dict, 2, self.rep3aes_bin, k3, m3, user_id, analysis_type, computation_id])
+
+        t1.start()
+        t2.start()
+        t3.start()
+        t1.join()
+        t2.join()
+        t3.join()
+
+        (nonce, ad) = prepare_params_for_dist_enc(MpcPartyKeys(TestDecryptKeyShare.get_config(0)), user_id, computation_id, analysis_type)
+        instance = AES.new(key=TestDecryptKeyShare.expected_key, mode=AES.MODE_GCM, nonce=nonce)
+        instance.update(ad)
+        expected_ct, expected_tag = instance.encrypt_and_digest(result_bytes)
+
+        # collect return values and check the ciphertext
+        cts = [return_dict[i] for i in range(3)]
+        for ct in cts:
+            assert ct.hex() == expected_ct.hex() + expected_tag.hex()
+
+    def run_dist_dec(return_val, party, path_to_bin, key_share, ct, user_id):
+        if party in [0,1,2]:
+            config = f'rep3aes/p{party+1}.toml'
+        else:
+            assert False
+        rep3aes_config = Rep3AesConfig(config, path_to_bin)
+        
+        message_share = dist_dec(rep3aes_config, user_id, key_share, ct)
+        return_val[party] =  message_share
+
+    def test_dist_dec(self):
+        user_id = "4d14750e-2353-4d30-ac2b-e893818076d2"
+        # create key shares
+        k1, k2, k3 = TestRep3Aes.secret_share(TestDecryptKeyShare.expected_key)
+
+        # create a message of 187 64-bit values in little endian
+        message = secrets.token_bytes(187 * 8)
+        nonce = bytes.fromhex('157316abe528fe29d4716781')
+        ad = bytes(user_id, encoding='utf-8') + nonce
+        instance = AES.new(key=TestDecryptKeyShare.expected_key, mode=AES.MODE_GCM, nonce=nonce)
+        instance.update(ad)
+        ct, tag = instance.encrypt_and_digest(message)
+        final_ct = nonce + ct + tag
+
+        return_dict = dict()
+        t1 = Thread(target=TestRep3Aes.run_dist_dec, args=[return_dict, 0, self.rep3aes_bin, k1, final_ct, user_id])
+        t2 = Thread(target=TestRep3Aes.run_dist_dec, args=[return_dict, 1, self.rep3aes_bin, k2, final_ct, user_id])
+        t3 = Thread(target=TestRep3Aes.run_dist_dec, args=[return_dict, 2, self.rep3aes_bin, k3, final_ct, user_id])
+
+        t1.start()
+        t2.start()
+        t3.start()
+        t1.join()
+        t2.join()
+        t3.join()
+
+        # reconstruct message shares
+        m1 = return_dict[0]
+        m2 = return_dict[1]
+        m3 = return_dict[2]
+        assert len(m1) == len(message)
+        assert len(m2) == len(message)
+        assert len(m3) == len(message)
+
+        reconstructed_message = bytearray(len(message))
+        for i in range(len(message)):
+            reconstructed_message[i] = m1[i] ^ m2[i] ^ m3[i]
+        assert message.hex() == reconstructed_message.hex()
+
 
 
 if __name__ == '__main__':
