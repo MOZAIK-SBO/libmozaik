001bff137f35a87471871d4d9d13cd8e59bd8b4d
Maffo1408 <mato1408@gmail.com>
2024-04-16 14:51:59 +0200

    Added script for setting config of the servers

---
+++ mpc/setup_config.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Function to print green message
+print_green() {
+  echo -e "\e[32m$1\e[0m"
+}
+
+# Function to print red message and exit
+print_red_and_exit() {
+  echo -e "\e[31m$1\e[0m"
+  exit 1
+}
+
+# Check if party_id parameter is provided
+if [ -z "$1" ]; then
+  print_red_and_exit "Usage: $0 <party_id>"
+fi
+
+# Change directory to mpc/rep3aes or exit with error message
+cd rep3aes || print_red_and_exit "Failed to change directory"
+
+# Run cargo build --verbose
+print_green "Building rep3aes"
+cargo build --verbose || print_red_and_exit "Cargo build failed"
+
+# Change directory to MP-SPDZ
+cd ../MP-SPDZ || print_red_and_exit "Failed to change directory"
+
+# Write HOSTS file
+print_green "Writing HOSTS file"
+printf "10.10.168.46:8000\n10.10.168.47:8000\n10.10.168.48:8000\n10.10.168.49:8000\n" > HOSTS
+
+# Build malicious-rep-ring-party.x
+print_green "Building malicious-rep-ring-party.x"
+make malicious-rep-ring-party.x || print_red_and_exit "Failed to build malicious-rep-ring-party.x"
+
+# Compile using compile.py
+print_green "Compiling heartbeat_inference_demo"
+./compile.py -R64 heartbeat_inference_demo || print_red_and_exit "Compilation failed"
+
+# Move deployment keys to rep3aes/keys directory
+print_green "Moving deployment keys to rep3aes/keys directory"
+mv ../rep3aes-deployment-keys/p* ../rep3aes/keys/ || print_red_and_exit "Failed to move deployment keys"
+
+# Change directory to rep3aes
+cd ../rep3aes || print_red_and_exit "Failed to change directory"
+
+# Write TOML configuration file for all parties
+party_index=$(( $1 + 1 ))
+print_green "Writing TOML configuration file for party $1"
+cat << EOF > "p${1}.toml"
+party_index = $party_index
+
+[p1]
+address = "10.10.168.46"
+port = 8100
+certificate = "keys/p1.pem"
+private_key = "keys/p1.key"
+
+[p2]
+address = "10.10.168.47"
+port = 8101
+certificate = "keys/p2.pem"
+private_key = "keys/p2.key"
+
+[p3]
+address = "10.10.168.48"
+port = 8102
+certificate = "keys/p3.pem"
+private_key = "keys/p3.key"
+EOF
+
+# Run test_mpc_deployment script
+print_green "Running test_mpc_deployment script for party $1"
+python3 test_mpc_deployment.py "$1" || print_red_and_exit "Failed to run test_mpc_deployment.py"
