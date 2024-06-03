485523f00cea7c2af1c677971d77dce90b4bc254
Leonard Schild <schild.leonard@gmail.com>
2024-06-03 18:40:49 +0200

    update to binary and server

--- fhe/setup_server.sh
+++ fhe/setup_server.sh
@@ -1,5 +1,12 @@
 source ./install_openfhe || exit;
 mkdir build;
 cmake -B build
-cmake --build build --target all || exit;
-mkdir ./SERVER/server_cache
+cmake --build build --parallel 8 || exit;
+
+mkdir -p ./SERVER/server_cache/bin;
+mkdir -p ./SERVER/server_cache/cache/keys;
+mkdir -p ./SERVER/server_cache/cache/models;
+mkdir -p ./SERVER/server_cache/cache/models/Heartbeat-Demo-1;
+
+cp build/fhe_server ./SERVER/server_cache/bin;
+cp assets/configs/* ./SERVER/server_cache/cache/models/Heartbeat-Demo-1/;
